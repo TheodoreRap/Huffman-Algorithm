c0970754ef2b45f555df330bfd20145c80ddc44a
TheodoreRap <103283373+TheodoreRap@users.noreply.github.com>
2022-06-11 17:40:31 +0300

    Update HuffmanMain.m

--- HuffmanMain.m
+++ HuffmanMain.m
@@ -43,7 +43,6 @@ end
 symbols(27)=char(32); % προσθέτω το κενό (space) σε ascii;
 
 %--------------------huffmandict-------------------------------------------
-[dict,avglen]=huffmandict(X,freq_prob); % έτοιμη συνάρτηση huffman
 [my_dict,len]=myHuffmanDict(symbols,freq_prob); 
 
 entropy=0; % υπολογισμός εντροπίας
@@ -51,7 +50,6 @@ for i = 1:27
     entropy=entropy-(freq_prob(i)*log2(freq_prob(i)));
 end
 
-n=entropy/avglen; % απόδοση βάσει την συνάρτησης huffmandict
 nn=entropy/len; % απόδοση βάσει την συνάρτησης myHuffmanDict
 
 %----------------------huffmanenco-----------------------------------------
@@ -83,71 +81,6 @@ for i=1:length(my_sig) % σύγκριση τιμών συναρτήσεων κα
         Success_deco=true;
     end
 end
-
-%---------------------new frequencies--------------------------------------
-eng_symbols=[];
-new_prob=[];
-i=1;
-file = fopen('frequencies.txt','r'); % άνοιγμα του txt αρχείου για ανάγνωση
-while true
-    tline = fgetl(file);
-    if ~ischar(tline), break, end
-    text=tline; % καταχώρηση του κειμένου γραμμή-γραμμή
-    text=split(text); % διαχωρισμός συμβόλου και πιθανότητας
-    eng_symbols=[eng_symbols,i]; % σύμβολα
-    new_prob=[new_prob,str2num(text{2})]; % αντίστοιχες πιθανότητες
-    i=i+1;
-end
-fclose(file);
-
-total_prob=0; % όλες οι πιθανότητες πρέπει να δίνουν άθροισμα 1
-for i=1:27
-    total_prob=total_prob+new_prob(i); % εδώ είναι 1.0003
-end
-% [dict,avglen]=huffmandict(eng_symbols,new_prob); % ΣΦΑΛΜΑ total_prob>1
-% μετά απο αλλαγές στον κώδικα βάζοντας >= αντί για == στην while ώστε 
-% να κάνει break και να μην βγάζει σφάλμα προκύπτει το new_dict και 
-% new_len με τα αντίστοιχα new_entropy και new_n
-% [new_dict,new_len]=myHuffmanDict(eng_symbols,new_prob); 
-% new_entropy=0; % υπολογισμός εντροπίας
-% for i = 1:27
-%     new_entropy=new_entropy-(new_prob(i)*log2(new_prob(i)));
-% end
-% new_n=new_entropy/new_len;
-
-%-------------------new source A-------------------------------------------
-% έχω 27 σύμβολα αρα θα προκύψουν 729 (27^2) νέα σύμβολα καταχωρημένα στο
-% cell array source2 που θα περιέχει τα καινουρια σύμβολα και τις 
-% αντίστοιχες καινούριες πιθανότητες
-N2=729;
-source2={1:729};
-k=1;
-for i=1:27
-    for j=1:27
-        source2(k,1)={symbols(i)+symbols(j)};
-        source2(k,2)={freq_prob(i)*freq_prob(j)};
-        k=k+1;
-    end
-end
-
-total_prob2=0;
-for i=1:N2 % το μήκος που προέκυψε είναι 729 και συνολική πιθανότητα 1
-    total_prob2=total_prob2+source2{i,2}; % άρα έγινε σωστή μετατροπή
-end
-prob2=zeros(1,N2);
-symbols2=zeros(1,N2);
-for i=1:N2
-    symbols2(i)=i;
-    prob2(i,:)=source2{i,2};
-end
-
-[dict2,len2]=myHuffmanDict(symbols2,prob2);
-
-entropy2=0; % υπολογισμός εντροπίας, πρέπει entropy2=2*entropy
-for i = 1:N2
-    entropy2=entropy2-(prob2(i)*log2(prob2(i)));
-end
-n2=entropy2/len2;
 
 %----------------------source B--------------------------------------------
 load("cameraman.mat");
@@ -199,6 +132,7 @@ i_entropy=0; % υπολογισμός εντροπίας
 for i = 1:i_len
     i_entropy=i_entropy-(i_prob(i)*log2(i_prob(i)));
 end
+
 i_n=i_entropy/i_avglen;
 
 i_testSample = randsrc(50,1,i_X); % δημιουργία 50 τυχαίων συμβόλων
@@ -213,45 +147,3 @@ for i=1:length(i_sig)
         Success_i=true;
     end
 end
-
-p_plot=zeros(1,99);
-HXY_plot=zeros(1,99); % H(X|Y)
-C_plot=zeros(1,99); % C=1-H(X|Y)
-k=1;
-for i=0.01:0.01:0.99
-    s=-i*log2(i)-(1-i)*log2(1-i); % υπολογισμός H(X|Y)
-    p_plot(k)=i;
-    HXY_plot(k)=s;
-    C_plot(k)=(1-s);
-    k=k+1;
-end
-
-x_y={i_len:2};
-sum_p=0;
-for j=1:10000   
-    counter=0;
-    for i=1:i_len % δημιουργία νέου cell array με πρώτη στήλη την ακολουθία
-        y=bsc(i_dict{i,2}); % που στέλνει ο x και δεύτερη στήλη την 
-        x_y(i,2)={y}; % ακολουθία που βλέπει ο y
-        x_y(i,1)=i_dict(i,2);
-    end
-    for i=1:i_len % έλεγχος σωστού αποτελέσματος x = y
-        if x_y{i,1}==x_y{i,2}
-            counter=counter+1;
-        end
-    end
-
-    sum_p=sum_p+counter/i_len;
-end
-
-probability=sprintf("%.2f",sum_p/10000); % ακρίβεια 2 δεκαδικών ψηφίων
-p=double(probability); 
-
-H_XY=-p*log2(p)-(1-p)*log2(1-p); % υπολογισμός H(X|Y)
-C=1-H_XY; % Χωρητικότητα καναλιού
-
-hold on % διάγραμμα σχέσης C - H(X|Y) όπου φαίνεται και η συμμετρία τους
-title("Συμμετρία C - H(X|Y)")
-plot(p_plot,C_plot,'red')
-plot(p_plot,HXY_plot,'blue')
-hold off
